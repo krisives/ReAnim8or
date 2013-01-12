d22b17df39aa8fcdf2044c1e84a7a3f59b4afd21
kris <kris@leetmint.(none)>
2013-01-11 22:07:41 -0800

    Added much better keyboard support.

--- js/Keyboard.js
+++ js/Keyboard.js
@@ -9,48 +9,91 @@ define(function () {
 		this.down = {};
 	}
 	
+	function combo(tokens) {
+		var ctrl = false, shift = false;
+		var keys = [];
+		
+		$.each(tokens, function (index, key) {
+			key = key.trim().toUpperCase();
+			if (key === 'CTRL') { ctrl = true; return; }
+			if (key === 'SHIFT') { shift = true; return; }
+			keys.push(key.charCodeAt(0));
+		});
+		
+		return function (e) {
+			var i, key;
+			if (keys.length <= 0) { return false; }
+			if (ctrl && !keyboard.ctrl) { return false; }
+			if (shift && !keyboard.shift) { return false; }
+			
+			for (i=0; i < keys.length; i++) {
+				key = keys[i];
+				
+				if (!keyboard.down[key]) {
+					return false;
+				}
+			}
+			
+			return true;
+		};
+	}
+	
+	function expression(str, checks) {
+		$.each(str.split('|'), function (index, part) {
+			var and = part.split('+');
+			if (and.length) { checks.push(combo(and)); }
+		});
+		
+		var len = checks.length;
+		
+		return function (e) {
+			var c, i;
+			
+			for (i=0; i < len; i++) {
+				c = checks[i];
+				if (c(e)) { return true; }
+			}
+			
+			return false;
+		};
+	}
+	
 	Keyboard.prototype = {
 		parse: function (expr, f) {
+			var or = [], check;
+			
 			if (typeof f === 'undefined') {
 				f = expr;
 				expr = null;
 			}
 			
-			var and = [];
-			
-			if (expr !== null) {
-				and = expr.split('+');
+			if (expr === null) {
+				return f;
 			}
 			
-			console.log(and);
+			check = expression(expr, or);
 			
 			return function (e) {
-				if (f) {
-					f(e);
-				}
+				if (!check(e)) { return; }
+				if (f) { f(e); }
 			};
 		},
 		
 		on: {
-			down: function (expr, f) {
-				f = keyboard.parse(expr, f);
-				$(window).keydown(f);
-			},
-			
-			up: function (expr, f) {
-				f = keyboard.parse(expr, f);
-				$(window).keyup(f);
-			}
+			down: function (expr, f) { $(window).keydown(keyboard.parse(expr, f)); },
+			up: function (expr, f) { $(window).keyup(keyboard.parse(expr, f)); }
 		}
 	};
 	
 	keyboard = new Keyboard();
 	
 	keyboard.on.down(function (e) {
+		keyboard.ctrl = keyboard.ctrl || e.ctrlKey;
 		keyboard.down[e.keyCode] = true;
 	});
 	
 	keyboard.on.up(function (e) {
+		keyboard.ctrl = keyboard.ctrl && e.ctrlKey;
 		keyboard.down[e.keyCode] = false;
 	});
 	
--- js/Tools/View.js
+++ js/Tools/View.js
@@ -86,7 +86,6 @@ define(['Tool', 'Mouse', 'Keyboard'], function (Tool, Mouse, Keyboard) {
 			}
 			
 			this.button.addClass('active');
-			console.log("Activate");
 		},
 		
 		deactivate: function () {
@@ -101,7 +100,6 @@ define(['Tool', 'Mouse', 'Keyboard'], function (Tool, Mouse, Keyboard) {
 			}
 			
 			this.button.removeClass('active');
-			console.log("Deactivate");
 		},
 		
 		pan: function () {
