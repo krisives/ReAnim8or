caefc744c0cd886a5ce36b5744157bf1ecebe579
Kristopher Ives <kris@leetdeb.(none)>
2013-01-14 20:16:35 -0500

    Started Cube tool

--- js/Tools/Cube.js
+++ js/Tools/Cube.js
@@ -1,5 +1,31 @@
 'use strict';
 
-define(['Tool'], function (Tool) {
+define(['Tool', 'Mouse'], function (Tool, Mouse) {
+	function CubeTool(editor, toolbar) {
+		var tool = this;
+		Tool.construct(this, 'cube', editor, toolbar);
+		this.button = toolbar.node.find('.tool-cube');
+		
+		Mouse.on.down(this.createHandler(function (e) {
+			console.log("down");
+		}));
+		
+		Mouse.on.move(this.createHandler(function (e) {
+			
+		}));
+		
+		Mouse.on.up(this.createHandler(function (e) {
+			console.log("up");
+		}));
+		
+		this.button.click(function (e) {
+			toolbar.changeTool(tool);
+		});
+	}
 	
+	CubeTool.prototype = Tool.extend({
+		
+	});
+	
+	return CubeTool;
 });
